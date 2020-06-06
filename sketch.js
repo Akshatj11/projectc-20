6eeb8f47aadfeca0ad0dcb431279339034c376d6
Akshatj11 <62606542+Akshatj11@users.noreply.github.com>
2020-06-06 11:40:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  
+}
+function draw() {
+  background(mouseX+50,mouseY+50,mouseX+50);
+  circle(mouseX,mouseY,50,20);
+  fill("black");
+  
+text(" I LOVE CODING",100,200);
+  textSize(32);
+  fill("pink");
+}
